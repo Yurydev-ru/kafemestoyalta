4b2ac3c270b6f8bbe7a1c6fb5ec948e5127695fa
Yury Works <yurydevru@gmail.com>
2026-02-26 05:30:14 +0100

    Added nav components

---
+++ app/components/layout/HeroSection.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="hero-section">
+    <div class="bg-video__wrapper">
+      <video class="bg-video" autoplay muted loop playsinline preload="auto">
+        <!-- eslint-disable-next-line vue/html-self-closing -->
+        <source src="/video/bg-video.mp4" type="video/mp4" />
+      </video>
+    </div>
+    <h1 class="title">Welcome to Our Website</h1>
+    <p class="subtitle">Discover amazing content and connect with us.</p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.hero-section {
+  position: relative;
+  color: #fff;
+  text-align: center;
+
+  .title {
+    font-size: 48px;
+    margin-bottom: 20px;
+  }
+
+  .subtitle {
+    font-size: 24px;
+  }
+}
+.bg-video__wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 16px;
+}
+.bg-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
---
+++ app/types/shared/CategoryCard.types.ts
@@ -0,0 +1,6 @@
+export interface CategoryCardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+  link: string;
+}
