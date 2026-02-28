a494407920954c88b8a1e232b861c439ee824bc1
Yury Works <yurydevru@gmail.com>
2026-02-28 06:49:19 +0100

    Added nav components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,56 +1,55 @@
 import { fileURLToPath } from "node:url";
 
 export default defineNuxtConfig({
-  devtools: { 
-    enabled: true
+  devtools: {
+    enabled: true,
   },
   nitro: {
     compressPublicAssets: true,
   },
   alias: {
-    '@ui': fileURLToPath(new URL('./components/ui', import.meta.url)),
-    '@layout': fileURLToPath(new URL('./components/layout', import.meta.url)),
-    '@feature': fileURLToPath(new URL('./components/feature', import.meta.url))
+    "@ui": fileURLToPath(new URL("./components/ui", import.meta.url)),
+    "@layout": fileURLToPath(new URL("./components/layout", import.meta.url)),
+    "@feature": fileURLToPath(new URL("./components/feature", import.meta.url)),
   },
-  css: ['~/assets/scss/main.scss'],
-  modules: ['@nuxt/icon'],
+  css: ["~/assets/scss/main.scss"],
+  modules: ["@nuxt/icon"],
   components: [
     {
-      path: '~/components/layout',
-      pathPrefix: false
+      path: "~/components/layout",
+      pathPrefix: false,
     },
     {
-      path: '~/components/ui',
-      pathPrefix: false
+      path: "~/components/ui",
+      pathPrefix: false,
     },
     {
-      path: '~/components/feature',
-      pathPrefix: false
+      path: "~/components/feature",
+      pathPrefix: false,
     },
   ],
   devServer: {
-    host: '127.0.0.1',
-    port: 3000
+    host: "127.0.0.1",
+    port: 3000,
   },
   typescript: {
     strict: true,
-    typeCheck: false
+    typeCheck: false,
   },
   icon: {
-    provider: 'iconify'
+    provider: "iconify",
   },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: ` 
-          @use "@/assets/scss/tokens/breakpoints";
-          @use "@/assets/scss/abstracts/mixins";
+          
           @use "@/assets/scss/tokens/colors";
-          `
-        }
-      }
-    }
+          `,
+        },
+      },
+    },
   },
-  compatibilityDate: '2025-07-15',
-})
+  compatibilityDate: "2025-07-15",
+});
