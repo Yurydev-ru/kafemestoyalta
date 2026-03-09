edd8ca25e7922880429bb48cab2a45b09da002d6
Yury Works <yurydevru@gmail.com>
2026-03-09 23:45:59 +0100

    Updated components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,30 +4,48 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
+  ssr: false,
+
+  runtimeConfig: {
+    public: {
+      siteName: "Kafe Mesto",
+    },
+  },
+
+  app: {
+    head: {
+      title: "Кафе Место",
+      titleTemplate: "%s | Кафе Место",
+      meta: [
+        { name: "description", content: "Лаундж кафе в центре Амстердама" },
+      ],
+      //   link: [
+      //     { rel: "icon", href: "/favicon.ico" },
+      //     { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      //   ],
+    },
+  },
   nitro: {
     compressPublicAssets: true,
   },
+
   alias: {
     "@ui": fileURLToPath(new URL("./components/ui", import.meta.url)),
     "@layout": fileURLToPath(new URL("./components/layout", import.meta.url)),
     "@feature": fileURLToPath(new URL("./components/feature", import.meta.url)),
+    "@assets": fileURLToPath(new URL("./assets", import.meta.url)),
+    "@composables": fileURLToPath(new URL("./composables", import.meta.url)),
+    "@utils": fileURLToPath(new URL("./utils", import.meta.url)),
+    "@store": fileURLToPath(new URL("./store", import.meta.url)),
+    "@pages": fileURLToPath(new URL("./pages", import.meta.url)),
+    "@layouts": fileURLToPath(new URL("./layouts", import.meta.url)),
+    "@components": fileURLToPath(new URL("./components", import.meta.url)),
+    "@modules": fileURLToPath(new URL("./modules", import.meta.url)),
   },
   css: ["~/assets/scss/main.scss"],
+
   modules: ["@nuxt/icon"],
-  components: [
-    {
-      path: "~/components/layout",
-      pathPrefix: false,
-    },
-    {
-      path: "~/components/ui",
-      pathPrefix: false,
-    },
-    {
-      path: "~/components/feature",
-      pathPrefix: false,
-    },
-  ],
+
   devServer: {
     host: "127.0.0.1",
     port: 3000,
@@ -46,8 +64,8 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData: ` 
-          @use "@/assets/scss/tokens/colors";
-          @use "@/assets/scss/abstracts/_mixins.scss" as *;
+          @use "~/assets/scss/tokens/colors" as *;
+          @use "~/assets/scss/abstracts/_mixins.scss" as *;
           `,
         },
       },
