0213a5fcd32a5bc061972f07f8aef1e2fb7fb382
Yury Works <yurydevru@gmail.com>
2026-03-25 21:08:05 +0100

    Fix nuxt-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,20 +46,38 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
+  //   alias: {
+  //     "@ui": fileURLToPath(new URL("./components/ui", import.meta.url)),
+  //     "@layout": fileURLToPath(new URL("./components/layout", import.meta.url)),
+  //     "@feature": fileURLToPath(new URL("./components/feature", import.meta.url)),
+  //     "@assets": fileURLToPath(new URL("./assets", import.meta.url)),
+  //     "@composables": fileURLToPath(new URL("./composables", import.meta.url)),
+  //     "@utils": fileURLToPath(new URL("./utils", import.meta.url)),
+  //     "@store": fileURLToPath(new URL("./store", import.meta.url)),
+  //     "@pages": fileURLToPath(new URL("./pages", import.meta.url)),
+  //     "@layouts": fileURLToPath(new URL("./layouts", import.meta.url)),
+  //     "@components": fileURLToPath(new URL("./components", import.meta.url)),
+  //     "@modules": fileURLToPath(new URL("./modules", import.meta.url)),
+  //   },
   alias: {
-    "@ui": fileURLToPath(new URL("./components/ui", import.meta.url)),
-    "@layout": fileURLToPath(new URL("./components/layout", import.meta.url)),
-    "@feature": fileURLToPath(new URL("./components/feature", import.meta.url)),
-    "@assets": fileURLToPath(new URL("./assets", import.meta.url)),
-    "@composables": fileURLToPath(new URL("./composables", import.meta.url)),
-    "@utils": fileURLToPath(new URL("./utils", import.meta.url)),
-    "@store": fileURLToPath(new URL("./store", import.meta.url)),
-    "@pages": fileURLToPath(new URL("./pages", import.meta.url)),
-    "@layouts": fileURLToPath(new URL("./layouts", import.meta.url)),
-    "@components": fileURLToPath(new URL("./components", import.meta.url)),
-    "@modules": fileURLToPath(new URL("./modules", import.meta.url)),
+    "@": fileURLToPath(new URL("./app", import.meta.url)),
+    "@app": fileURLToPath(new URL("./app", import.meta.url)),
+    "@components": fileURLToPath(new URL("./app/components", import.meta.url)),
+    "@ui": fileURLToPath(new URL("./app/components/ui", import.meta.url)),
+    "@layout": fileURLToPath(
+      new URL("./app/components/layout", import.meta.url),
+    ),
+    "@feature": fileURLToPath(
+      new URL("./app/components/feature", import.meta.url),
+    ),
+    "@composables": fileURLToPath(
+      new URL("./app/composables", import.meta.url),
+    ),
+    "@assets": fileURLToPath(new URL("./app/assets", import.meta.url)),
+    "~/": fileURLToPath(new URL("./app", import.meta.url)),
+    "@/": fileURLToPath(new URL("./app", import.meta.url)),
   },
-  css: ["~/assets/scss/main.scss"],
+  css: ["@assets/scss/main.scss"],
 
   modules: ["@nuxt/icon"],
 
@@ -81,11 +99,14 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData: ` 
-          @use "~/assets/scss/tokens/colors" as *;
-          @use "~/assets/scss/abstracts/_mixins.scss" as *;
+          @use "@assets/scss/tokens/colors" as *;
+          @use "@assets/scss/abstracts/_mixins.scss" as *;
           `,
         },
       },
+    },
+    optimizeDeps: {
+      include: ["@vue/devtools-core", "@vue/devtools-kit"],
     },
   },
   compatibilityDate: "2025-07-15",
